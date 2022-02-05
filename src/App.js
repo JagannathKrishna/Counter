9676420a3d88da38310d56f04b6a9b8089b35518
Jagannath Krishna.P.A <jagannathkrishnapa@gmail.com>
2022-02-05 13:01:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {  useState } from 'react'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, setstate] = useState(0)
+  const handleKeyPress = (event) => {
+    if(event.key === 'Space'){
+      setstate(state+1)
+    }
 }
+    return (
+        <div className='App'>
+          <div className='btns'>
+            <button type="button" class="btn btn-primary" onClick={()=>setstate(state+1)}>Increment</button>
+            <button type="button" class="btn btn-primary" onClick={()=>setstate(state-1)}>Decrement</button>
+            
+          </div>
+            <h1 className='counter' onKeyPress={handleKeyPress} >{state}</h1>
+        </div>
+    )
+}
+
+
+
+
 
 export default App;
